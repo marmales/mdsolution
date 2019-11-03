e4f8389e7fed3e77252c78f8f5434e92bccab221
marmales <mar.maleszewski@gmail.com>
2019-11-03 10:27:39 +0100

    Updates

---
+++ precache-manifest.1557c580afe10e39c3302b6fd8f614ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d97d0c2e9beb19fe5a81c48289f0908",
+    "url": "/mdsolution/index.html"
+  },
+  {
+    "revision": "0f6a297556a77518b65a",
+    "url": "/mdsolution/static/js/2.2c1b6007.chunk.js"
+  },
+  {
+    "revision": "6adeacd6d830397c1d49",
+    "url": "/mdsolution/static/js/main.39952494.chunk.js"
+  },
+  {
+    "revision": "0c834ae8f5ed336156d2",
+    "url": "/mdsolution/static/js/runtime~main.90297585.js"
+  },
+  {
+    "revision": "4ab4aa2e4739c03d077fae9f7cb366a7",
+    "url": "/mdsolution/static/media/boar.4ab4aa2e.png"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/mdsolution/static/media/javascript.dd60b11e.svg"
+  },
+  {
+    "revision": "b554c48126c48861cff8f431638a7baf",
+    "url": "/mdsolution/static/media/linkedin.b554c481.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9a9593af4f7a9a375f8fbe7c268ce40.js"
+  "/mdsolution/precache-manifest.1557c580afe10e39c3302b6fd8f614ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mdsolution/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
